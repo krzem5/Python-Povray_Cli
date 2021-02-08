a46cf8e51a0c2f2a383bb0cb5c09e728d08a81a5
Krzem <krzem5.dev@gmail.com>
2021-02-08 21:42:09 +0100

    Push Update 02/08/2021, 21:42:02

--- src/main.py
+++ src/main.py
@@ -3,10 +3,7 @@ import os
 
 
 
-EXE_FILE_PATH="D:\\K\\Downloads\\povray\\povray\\windows\\vs10\\bin64\\povconsole64.exe"
-
-
-
 with open("./__tmp.ini","w") as f:
-	f.write("Library_Path=C:\\Users\\aleks\\AppData\\Local\\Temp\\__povray\\include\nWidth=200\nHeight=200\nOutput_File_Name=__out.tmp\nDisplay=true\nInput_File_Name=C:\\Users\\aleks\\AppData\\Local\\Temp\\__povray\\scenes\\subsurface\\subsurface.pov\nOutput_To_File=true\n")
-subprocess.run([EXE_FILE_PATH,"__tmp.ini"])
+	f.write("Library_Path=C:\\Users\\aleks\\AppData\\Local\\Temp\\__povray\\include\nWidth=200\nHeight=200\nOutput_File_Name=__out.tmp\nInput_File_Name=C:\\Users\\aleks\\AppData\\Local\\Temp\\__povray\\scenes\\subsurface\\subsurface.pov\nOutput_To_File=true\n")
+subprocess.run(["D:\\K\\Downloads\\povray\\povray\\windows\\vs10\\bin64\\povconsole64.exe","__tmp.ini"])
+os.remove("__tmp.ini")
