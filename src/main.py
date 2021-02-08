766c3bc663219ad2979dd89a480cb2999bb4fd7f
Krzem <krzem5.dev@gmail.com>
2021-02-08 21:36:16 +0100

    Push Update 02/08/2021, 21:36:10

--- src/main.py
+++ src/main.py
@@ -1,16 +1,12 @@
 import subprocess
 import os
-import ctypes
 
 
 
 EXE_FILE_PATH="D:\\K\\Downloads\\povray\\povray\\windows\\vs10\\bin64\\povconsole64.exe"
-GENERIC_READ=0x80000000
 
 
 
 with open("./__tmp.ini","w") as f:
-	f.write("Width=400\nHeight=400\nOutput_File_Name=__out.tmp\n")
-# p=subprocess.Popen([EXE_FILE_PATH,"C:\\Users\\aleks\\AppData\\Local\\Temp\\__povray\\include","__tmp.ini","C:\\Users\\aleks\\AppData\\Local\\Temp\\__povray\\scenes\\subsurface\\subsurface.pov"])
-fh=ctypes.windll.kernel32.CreateFileW("__out.tmp.pov-state",GENERIC_READ,FILE_SHARE_READ,NULL,OPEN_EXISTING,FILE_FLAG_OVERLAPPED|FILE_FLAG_NO_BUFFERING,NULL)
-ctypes.windll.kernel32.CloseHandle(fh)
+	f.write("Library_Path=C:\\Users\\aleks\\AppData\\Local\\Temp\\__povray\\include\nWidth=200\nHeight=200\nOutput_File_Name=__out.tmp\nDisplay=true\nInput_File_Name=C:\\Users\\aleks\\AppData\\Local\\Temp\\__povray\\scenes\\subsurface\\subsurface.pov\nOutput_To_File=true\n")
+subprocess.run([EXE_FILE_PATH,"__tmp.ini"])
